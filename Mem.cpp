c7633df5b3fe18275bfa8e2054d05cc30c268097
sinlnw <sinlnw4567@gmail.com>
2023-08-05 12:45:51 +0700

    add comment

--- Mem.cpp
+++ Mem.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <cstdlib>
 
-int global_num1 =1112;
-int global_num2 =1450;
-int global_num3 =1550;
-int test = 0;
+int global_num1 =1112; // Initialized global variable in Initialized data segment
+int global_num2 =1450; // Initialized global variable in Initialized data segment
+int global_num3 =1550; // Initialized global variable in Initialized data segment
+int test = 0; 
 
 
 void call_stack(int num)
@@ -13,7 +13,7 @@ void call_stack(int num)
     {
         return;
     }
-    int numnum = num;
+    int numnum = num; // local variable in stack
     std::cout<<" stack"<<num+1<<" : "<<&numnum<<"\n";
     numnum++;
     call_stack(numnum);
@@ -25,7 +25,7 @@ int main(void)
     
     for (int i = 0; i < 3; i++)
     {
-        int_pointer_array[i] = (int*)malloc(sizeof(int)*3);
+        int_pointer_array[i] = (int*)malloc(sizeof(int)*3); // local variable in heap
         
     }
 
@@ -33,7 +33,7 @@ int main(void)
 
     for (int i = 2; i >= 0; i--)
     {
-        std::cout<<"malloc"<<i+1<<" : "<<int_pointer_array[i]<<"\n";
+        std::cout<<"malloc"<<i+1<<" : "<<int_pointer_array[i]<<"\n"; // dynamically allocated memory in heap
         free(int_pointer_array[i]);
     }
 
